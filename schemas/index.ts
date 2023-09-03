0f19b529c1adb8fd86b8da40cdda73f4830bfbea
JordanGallant <39-12602@sae.edu.za>
2023-09-03 19:58:26 +0200

    init

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,2 +1,6 @@
 import user from './user'
-export const schemaTypes = [user]
+import pin from './pin'
+import save from './save'
+import comment from './comment'
+import postedBy from './postedBy'
+export const schemaTypes = [user,pin,save,comment,postedBy]
